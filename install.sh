ac6340b62ec5d5e58889997152ba2d747d18de36
Tommy Bohde <accounts2952@gmail.com>
2016-04-08 15:34:04 -0400

    Update install.sh
    fixed a previous inconsistent update (package referenced was not package downloaded)

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@
 cd /tmp
 wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20150131-5_all.deb
 apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1
-dpkg -i raspi-config_20121028_all.deb
+dpkg -i raspi-config_20150131-5_all.deb
