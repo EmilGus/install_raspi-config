6d858f0c7ce869664e5fc4a5765c03012c5bc910
Ezra <ezraholm50@gmail.com>
2016-09-07 23:01:31 +0200

    Redone the script
    Added some nice features including root check and check if its already installed. Also added auto install of dependancies since i had to install alsautils on ubuntu but it had another package name.

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
+# Check if root
+if [ "$(whoami)" != "root" ]; then
+  whiptail --msgbox "Sorry you are not root. You must type: sudo sh install.sh" $WT_HEIGHT $WT_WIDTH
+  exit
+fi
 
-# must run as root or use sudo
+# Check if raspi-config is installed
+if [ $(dpkg-query -W -f='${Status}' raspi-config 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
+  whiptail --msgbox "Raspi-config is already installed, try upgrading it within raspi-config..." 10 60
+else
+  wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20160527_all.deb -P /tmp
+  apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils -y
+  # Auto install dependancies on eg. ubuntu server on RPI
+  apt-get install -fy
+  dpkg -i /tmp/raspi-config_20160527_all.deb
+  whiptail --msgbox "Raspi-config is now installed, run it by typing: sudo raspi-config" 10 60
+fi
 
-cd /tmp
-wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20150131-5_all.deb
-apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1
-dpkg -i raspi-config_20150131-5_all.deb
+exit
